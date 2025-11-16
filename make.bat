dbed3ebdbe5bcb8297c849343224c4e609f74855
David Howland <dhowland@gmail.com>
2025-11-16 15:39:42 -0500

    Add make feedback

--- make.bat
+++ make.bat
@@ -13,15 +13,21 @@ echo usage: make.bat {colors, themes, install}
 goto end
 
 :colors
+@ECHO -- .\themes\palette.html
 python .\tools\palette.py .\themes
 goto end
 
 :themes
+@ECHO -- .\themes\Npp.xml
+@ECHO -- .\themes\Npp_colors.html
 python .\tools\stylers.py .\templates\stylers.template.xml .\themes\Npp.json .\themes\Npp.xml
+@ECHO -- .\themes\Action.xml
+@ECHO -- .\themes\Action_colors.html
 python .\tools\stylers.py .\templates\stylers.template.xml .\themes\Action.json .\themes\Action.xml
 goto end
 
 :install
+@ECHO -- %APPDATA%\Notepad++\themes\Action.xml
 copy ".\themes\Action.xml" "%APPDATA%\Notepad++\themes\"
 goto end
 
